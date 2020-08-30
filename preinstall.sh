ddac9c87c397a66c2297d2b29e63686544e7590b
czM1K3 <michal.madera@madsoft.cz>
2020-08-30 11:05:13 +0200

    fixed

--- preinstall.sh
+++ preinstall.sh
@@ -70,17 +70,16 @@ echo "--------------------------------------"
 echo "-- Bootloader Systemd Installation  --"
 echo "--------------------------------------"
 bootctl --path=/boot install
-cat <<EOF > /boot/loader/entries/arch.conf
-title Arch Linux  
+printf 'title Arch Linux  
 linux /vmlinuz-linux  
 initrd  /initramfs-linux.img  
-options root=${DISK}1 rw
-EOF
+options root=${DISK}1 rw' > /boot/loader/entries/arch.conf
+
 
 echo "--------------------------------------"
 echo "--          Network Setup           --"
 echo "--------------------------------------"
-pacman -S network-manager dhclient --noconfirm --needed
+pacman -S networkmanager dhclient --noconfirm --needed
 systemctl enable --now NetworkManager
 
 echo "--------------------------------------"
