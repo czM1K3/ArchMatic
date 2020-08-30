40b397b55c1e09f5986cbc30388befc559a8c1d4
czM1K3 <michal.madera@madsoft.cz>
2020-08-30 10:16:14 +0200

    changed test

--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
+timedatectl set-ntp true
+pacman -S --noconfirm pacman-contrib
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 curl -s "https://www.archlinux.org/mirrorlist/?country=CZ&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
