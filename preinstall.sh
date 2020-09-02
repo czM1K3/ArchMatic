10c70ea1c42314d1884a132b13f8993297fe4615
czM1K3 <michal.madera@madsoft.cz>
2020-09-02 07:53:43 +0200

    fix

--- preinstall.sh
+++ preinstall.sh
@@ -68,7 +68,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 wget https://raw.githubusercontent.com/czM1K3/ArchMatic/master/bootinstall.sh -P /mnt
 
 echo "sh bootinstall.sh"
-echo sh bootinstall.sh | arch-chroot /mnt
+arch-chroot /mnt
 
 rm /mnt/bootinstall.sh
 
